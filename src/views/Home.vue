8d864c02752aba3158cd052eefbde66fe8eba429
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-06-19 20:05:19 +0300

    create home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,67 @@
 <template>
-  <hello-world />
+  <v-container>
+    <div class="text-h3">List of Posts</div>
+    <v-row class="mt-3">
+      <v-col v-if="!posts.length"><div class="text-h4">Sorry we don't have any posts</div></v-col>
+      <v-col cols="12" md="4" xl="3" v-for="post in posts" :key="post.id">
+        <v-card
+          class="mx-auto"
+          outlined
+        >
+          <v-list-item three-line>
+            <v-list-item-content>
+              <!-- <div class="text-overline mb-4">
+                OVERLINE
+              </div> -->
+              <v-list-item-title class="text-h5 mb-1">
+                {{ post.title }}
+              </v-list-item-title>
+              <v-list-item-subtitle>{{ post.body }}</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-avatar
+              tile
+              size="80"
+              color="grey"
+            ></v-list-item-avatar>
+          </v-list-item>
+
+          <v-card-actions>
+            <v-btn
+              outlined
+              rounded
+              text
+            >
+              Button
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
 
 export default {
   name: 'Home',
 
-  components: {
-    HelloWorld
+  data () {
+    return {
+      posts: []
+    }
+  },
+
+  methods: {
+    getPosts () {
+      this.axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+        this.posts = res.data
+      })
+    }
+  },
+
+  mounted () {
+    this.getPosts()
   }
 }
 </script>
