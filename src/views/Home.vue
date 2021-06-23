f03cafe6dc8832226fa20d4f8625aedb0fcfd8e2
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-06-23 19:24:48 +0300

    event bus

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,37 @@
 <template>
   <v-container>
-    <div class="text-h3">List of Posts</div>
+    <div class="text-h3" @click="bus.$emit('Home:loaded')">List of Posts</div>
     <v-row class="mt-3">
       <v-col v-if="!posts.length"><div class="text-h4">Sorry we don't have any posts</div></v-col>
       <v-col cols="12" md="4" xl="3" v-for="post in posts" :key="post.id">
-        <v-card
-          class="mx-auto"
-          outlined
-        >
-          <v-list-item three-line>
-            <v-list-item-content>
-              <!-- <div class="text-overline mb-4">
-                OVERLINE
-              </div> -->
-              <v-list-item-title class="text-h5 mb-1">
-                {{ post.title }}
-              </v-list-item-title>
-              <v-list-item-subtitle>{{ post.body }}</v-list-item-subtitle>
-            </v-list-item-content>
-
-            <v-list-item-avatar
-              tile
-              size="80"
-              color="grey"
-            >
-              <v-img :src="`https://picsum.photos/id/${post.id}/80/80`" />
-            </v-list-item-avatar>
-          </v-list-item>
-
-          <v-card-actions>
-            <v-btn
-              outlined
-              rounded
-              text
-              @click="$router.push({ name: 'ShowPost', params: { id: post.id } })"
-            >
-              Read More
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+        {{ post.title }}
+        <post-card :post.sync="localPost"></post-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import PostCard from '@/components/PostCard'
+import Bus from '@/plugins/event-bus'
 
 export default {
   name: 'Home',
 
+  components: {
+    PostCard
+  },
+
   data () {
     return {
-      posts: []
+      posts: [],
+      localPost: {
+        id: 10,
+        title: 'hi',
+        body: 'heelo',
+        userId: 4
+      },
+      bus: Bus
     }
   },
 
@@ -60,11 +40,20 @@ export default {
       this.axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
         this.posts = res.data
       })
+    },
+
+    changeTitle (id, title) {
+      const i = this.posts.findIndex((post) => post.id === id)
+      this.posts[i].title = title
     }
   },
 
   mounted () {
     this.getPosts()
+
+    Bus.$on('PostCard:changeTitle', () => {
+      console.log('I am here')
+    })
   }
 }
 </script>
