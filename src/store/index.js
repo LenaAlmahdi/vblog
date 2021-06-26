358abd59f16b5e2759cbb02d2236acab64598e77
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2021-06-26 19:08:34 +0300

    add edit post view

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,34 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    drawer: false
+    drawer: false,
+    darkMode: false
   },
   mutations: {
+
     toggleDrawer (state) {
       state.drawer = !state.drawer
+    },
+    toggleDarkMode (state) {
+      state.darkMode = !state.darkMode
+    },
+    Edit_Post (state, post) {
+      state.post.forEach(p => {
+        if (p.id === post.id) {
+          // eslint-disable-next-line no-unused-expressions
+          p - post
+        }
+      })
     }
   },
   actions: {
+    async editPost ({ commit }, post) {
+      // eslint-disable-next-line no-undef
+      const response = await Api().put(`/posts/${post.id}`, post)
+      const newPost = response.data.data.attributes
+      commit('Edit_Post', newPost)
+      return newPost
+    }
   },
   modules: {
   }
---
+++ src/views/posts/EditPost.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <div class="text-h3">The Edit Post View</div>
+    {{ post.title }}
+    <v-form @submit.prevent="editPost">
+      <v-text-field v-model="post.title" label="Post Title" />
+      <v-text-field v-model="post.body" label="Post Content" />
+      <v-btn type="submit">Edit Post</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+// import { mapState } from 'vuex'
+
+export default {
+  props: ['postId'],
+
+  name: 'EditPost',
+
+  data () {
+    return {
+      post: {}
+    }
+  },
+
+  methods: {
+    loadPost () {
+      this.axios.get(`https://jsonplaceholder.typicode.com/posts/${this.postId}`).then((res) => {
+        this.post = res.data
+      })
+    },
+    editPost () {
+      const payload = {
+        title: this.post.title,
+        body: this.post.body
+      }
+      this.axios.put(`https://jsonplaceholder.typicode.com/posts/${this.postId}`, payload).then((res) => {
+        alert('hello evrything is going well')
+      })
+    }
+  },
+
+  mounted () {
+    this.loadPost()
+  }
+
+}
+</script>
